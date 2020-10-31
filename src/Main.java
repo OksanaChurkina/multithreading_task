af8c2b0a5696a4b8e08a83b58ea0175c1cab52ab
Oksana Churkina <ochurkina@parseq.pro>
2020-10-31 22:22:01 +0300

    Simple implementation

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,52 @@
 import model.CalculateResult;
 import model.DownloadResult;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         Calendar start = Calendar.getInstance();
 
         int finishCounter = 0;
-        for (int i = 0; i < 1000; i++) {
 
+        ExecutorService downloadService = Executors.newFixedThreadPool(10) ;
+        ExecutorService calculateService = Executors.newFixedThreadPool(10) ;
+
+        List<Future<DownloadResult>> downloadList = new ArrayList<>();
+        List<Future<CalculateResult>> calculateList = new ArrayList<>();
+        //23sec
+        for(int i = 0; i< 100; i++){
             Download d = new Download(i);
-            DownloadResult downloadResult = d.downloadNext();
+            Future downloadResultFuture = downloadService.submit(d);
+            downloadList.add(downloadResultFuture);
+        }
 
-            Calculate c = new Calculate(downloadResult);
-            CalculateResult calculateResult = c.calculate();
+        downloadService.shutdown();
 
-            if (calculateResult.found) {
+//        for(int i = 0; i< 3000; i++){
+//            Download d = new Download(i);
+//            Calculate c = new Calculate(d.downloadNext());
+//            Future calcResultFuture = calculateService.submit(c);
+//            calculateList.add(calcResultFuture);
+//        }
+        for(Future<DownloadResult> d : downloadList) {
+            calculateList.add(calculateService.submit(new Calculate(d.get())));
+        }
+
+        for(Future<CalculateResult> calculateResultFuture : calculateList){
+            if(calculateResultFuture.get().found){
                 finishCounter++;
             }
         }
+        calculateService.shutdown();
 
         System.out.println("Total success checks: " + finishCounter);
 
